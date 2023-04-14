c250b914617903c1de770f90a34c6369f71a5141
Rick Boshae <rwboshae@gmail.com>
2023-04-14 00:22:25 -0700

    Add recurse option

--- run.py
+++ run.py
@@ -2,8 +2,8 @@ import argparse
 import os
 from pathlib import Path
 
-from plex_media_organizer import PlexMediaOrganizer
 import keys
+from plex_media_organizer import PlexMediaOrganizer
 
 def main():
     # Parse command-line arguments
@@ -11,14 +11,29 @@ def main():
     parser.add_argument('path', type=Path, help='path to the directory containing the media files')
     parser.add_argument('--guess', action='store_true', help='guess movie titles if not found on OMDB')
     parser.add_argument('--silent', action='store_true', help='run without user input')
+    parser.add_argument('--recursive', action='store_true', help='recurse into subdirectories')
     args = parser.parse_args()
 
     # Initialize PlexMediaOrganizer object
     organizer = PlexMediaOrganizer(api_key=os.environ.get('OMDB_API_KEY') or keys.OMDB_API_KEY)
 
-    # Organize media files in the given directory
-    organizer.organize_directory(args.path, guess=args.guess, silent=args.silent)
+    if not os.path.exists(args.path):
+        print(f"Error: Path {args.path} does not exist")
+        return
 
+    if os.path.isfile(args.path):
+        organizer.rename_movie_files(args.path)
+    else:
+        files = []
+        if args.recurse:
+            for (dirpath, _, filenames) in os.walk(args.path):
+                for filename in filenames:
+                    files.append(os.path.join(dirpath, filename))
+        else:
+            files = [os.path.join(args.path, f) for f in os.listdir(args.path) if os.path.isfile(os.path.join(directory, f))]
+
+            for f in files:
+                organizer.rename_movie_files(f, guess=args.guess, silen=args.silent)
 
 if __name__ == '__main__':
     main()
