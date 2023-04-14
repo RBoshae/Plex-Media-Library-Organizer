5fdd65f6eb24f9396f0a279c85c1f08659971e3a
Rick Boshae <rwboshae@gmail.com>
2023-04-13 23:25:53 -0700

    Add config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+# A list of common strings found in filenames that should be removed to improve
+# the accuracy of the movie title guess function.# This file should be edited 
+# as needed, and changes will be persistent across runs of the program.
+COMMON_STRINGS_FILE = "common_strings.txt"
